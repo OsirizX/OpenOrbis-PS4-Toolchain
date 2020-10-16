279991b7ca133a868601409c72d4a6112184eb35
OsirizX <osirisx99@gmail.com>
2020-10-15 18:31:37 -0700

    fix stat.h

--- include/bits/stat.h
+++ include/bits/stat.h
@@ -1,22 +1,26 @@
 /* copied from kernel definition, but with padding replaced
  * by the corresponding correctly-sized userspace types. */
 
-struct stat {
-	dev_t st_dev;
-	ino_t st_ino;
-	nlink_t st_nlink;
+#include <stdint.h>
 
-	mode_t st_mode;
+struct stat {
+	uint32_t st_dev;
+	uint32_t st_ino;
+	uint16_t st_mode;
+	nlink_t st_nlink;
 	uid_t st_uid;
 	gid_t st_gid;
-	unsigned int    __pad0;
 	dev_t st_rdev;
-	off_t st_size;
-	blksize_t st_blksize;
-	blkcnt_t st_blocks;
-
 	struct timespec st_atim;
 	struct timespec st_mtim;
 	struct timespec st_ctim;
-	long __unused[3];
+	off_t st_size;
+	blkcnt_t st_blocks;
+	blksize_t st_blksize;
+	uint32_t st_flags;
+	uint32_t st_gen;
+	int32_t st_lspare;
+	struct timespec st_birthtim;
+	unsigned int :(8 / 2) * (16 - (int)sizeof(struct timespec));
+	unsigned int :(8 / 2) * (16 - (int)sizeof(struct timespec));
 };
